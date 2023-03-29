eb3ae846630d3946a53a49cccf9b15623fe6ca82
sunil parmar <sunil04ce@gmail.com>
2023-03-29 22:57:46 +0530

    add layout, navbar component and images

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { theme } from "../chakra/theme";
+import Layout from "../components/layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
