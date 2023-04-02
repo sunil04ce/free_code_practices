f20ca122242506c8eea3d80732e50ab87d307ea9
sunil parmar <sunil04ce@gmail.com>
2023-04-02 22:40:25 +0530

    use atom of type AuthModalState

--- src/atoms/authModalAtom.ts
+++ src/atoms/authModalAtom.ts
@@ -1,3 +1,16 @@
 import { atom } from "recoil";
 
-export interface AuthModalState {}
+export interface AuthModalState {
+  open: boolean;
+  view: "login" | "signup" | "resetPassword";
+}
+
+const defaultModalState: AuthModalState = {
+  open: false,
+  view: "login",
+};
+
+export const authModalState = atom<AuthModalState>({
+  key: "authModalState",
+  default: defaultModalState,
+});
--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -4,32 +4,49 @@ import {
   ModalOverlay,
   ModalContent,
   ModalHeader,
-  ModalFooter,
   ModalBody,
   ModalCloseButton,
-  Button,
-  useDisclosure,
+  Flex,
 } from "@chakra-ui/react";
+import { useRecoilState } from "recoil";
+import { authModalState } from "@component/src/atoms/authModalAtom";
 
 const AuthModal: React.FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  // const { isOpen, onOpen, onClose } = useDisclosure();
+  const [modalState, setModalState] = useRecoilState(authModalState);
+
+  const closeHandler = () => {
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
+
   return (
     <>
-      <Button onClick={onOpen}>Open Modal</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={modalState.open} onClose={closeHandler}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>
+            {modalState.view === "login" && "Login"}
+            {modalState.view === "signup" && "Sign Up"}
+            {modalState.view === "resetPassword" && "Reset Password"}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>Here is a modal body</ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
-          </ModalFooter>
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              width="70%"
+              border="1px solid red"
+            ></Flex>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
