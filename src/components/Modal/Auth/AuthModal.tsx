6c33893f1e01532befc11185e4b2e542627f6b39
sunil parmar <sunil04ce@gmail.com>
2023-04-04 22:52:58 +0530

    change in Login modal

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -28,7 +28,7 @@ const AuthModal: React.FC = () => {
       <Modal isOpen={modalState.open} onClose={closeHandler}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>
+          <ModalHeader textAlign="center">
             {modalState.view === "login" && "Login"}
             {modalState.view === "signup" && "Sign Up"}
             {modalState.view === "resetPassword" && "Reset Password"}
@@ -39,13 +39,13 @@ const AuthModal: React.FC = () => {
             flexDirection="column"
             alignItems="center"
             justifyContent="center"
+            pb={6}
           >
             <Flex
               direction="column"
               align="center"
               justify="center"
               width="70%"
-              border="1px solid red"
             >
               {/* <OAuthButtons /> */}
               <AuthInputs />
--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -1,17 +1,84 @@
-import { Input } from "@chakra-ui/react";
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
+import { authModalState } from "@component/src/atoms/authModalAtom";
 import React, { useState } from "react";
+import { useSetRecoilState } from "recoil";
 
 type LoginProps = {};
 
 const Login: React.FC<LoginProps> = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
   const [loginForm, setLoginForm] = useState({
     email: "",
     password: "",
   });
+
+  const onSumbit = () => {};
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginForm((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   return (
-    <form>
-      <Input />
-      <Input />
+    <form onSubmit={onSumbit}>
+      <Input
+        required
+        name="email"
+        placeholder="email"
+        type="email"
+        mb={2}
+        onChange={onChange}
+        fontSize="12px"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
+      />
+      <Input
+        required
+        name="password"
+        placeholder="password"
+        type="password"
+        mb={2}
+        onChange={onChange}
+        fontSize="12px"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
+      />
+
+      <Button type="submit" width="100%" height="36px" mt={2} mb={2}>
+        Log In
+      </Button>
+      <Flex fontSize="9pt" justifyContent="center">
+        <Text mr={1}>New Here?</Text>
+        <Text
+          color="blue.500"
+          fontWeight={700}
+          cursor="pointer"
+          onClick={() =>
+            setAuthModalState((prev) => ({
+              ...prev,
+              view: "signup",
+            }))
+          }
+        >
+          SIGN UP
+        </Text>
+      </Flex>
     </form>
   );
 };
