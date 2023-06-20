2bb2b512f43cbed05785c635e4ac2d5a63fd5c44
sunil parmar <sunil04ce@gmail.com>
2023-06-20 22:34:04 +0530

    use of Outlet, added Navbar

--- react-router-6-tutorial/src/App.js
+++ react-router-6-tutorial/src/App.js
@@ -8,11 +8,13 @@ import Error from './pages/Error';
 function App() {
   return (
     <BrowserRouter>
+      <nav>Our navbar</nav>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='products' element={<Products />} />
-        <Route path='*' element={<Error />} />
+        <Route path='/' element={<Home />} >
+          <Route path='about' element={<About />} />
+          <Route path='products' element={<Products />} />
+          <Route path='*' element={<Error />} />
+        </Route>
 
         {/* <Route path='/' element={<div>Home page</div>} />
         <Route path='test'
@@ -20,6 +22,7 @@ function App() {
             <h2>Testing page</h2>
           </div>} /> */}
       </Routes>
+      <footer>Our footer</footer>
     </BrowserRouter>
   );
 }
